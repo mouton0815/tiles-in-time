e3387ccbccecec5c6789e5e68a7116fde68a273b
Torsten Schlieder <torsten.schlieder@mercateo.com>
2022-04-26 18:28:41 +0200

    Login only if necessary

--- src/login.js
+++ src/login.js
@@ -1,23 +1,37 @@
-import { until } from 'selenium-webdriver'
+import { By, until } from 'selenium-webdriver'
 import { sleep } from './sleep.js'
-import { getElementById, getElementByPath } from './locators.js'
+import { getElementById } from './locators.js'
 
 export async function login(driver, username, password) {
-    const updateButton = await getElementByPath(driver, '(//a[@href="/update"])[1]')
-    console.log('---> Update button located')
-    await updateButton.click()
+    const updateButtons = await driver.findElements(By.xpath('//a[./strong[text()="Log In"]]'))
+    if (updateButtons.length === 0) {
+        console.log('---> No Login button found, already logged in')
+    } else {
+        console.log('---> Login button located')
+        await updateButtons[0].click()
+        try {
+            // VeloViewer now routes to Strava
+            await driver.wait(until.titleContains('Strava'), 2000)
 
-    // VeloViewer now routes to Strava
-    await driver.wait(until.titleContains('Strava'))
-    const usernameField = await getElementById(driver, 'email')
-    await usernameField.sendKeys(username)
-    await sleep(1000) // TODO: Reconsider waiting
-    const passwordField = await getElementById(driver, 'password')
-    await passwordField.sendKeys(password)
-    await sleep(1000) // TODO: Reconsider waiting
-    const loginButton = await getElementById(driver, 'login-button')
-    await loginButton.click()
+            const usernameField = await getElementById(driver, 'email')
+            await usernameField.sendKeys(username)
+            await sleep(1000) // Wait to simulate human behavior
+            const passwordField = await getElementById(driver, 'password')
+            await passwordField.sendKeys(password)
+            await sleep(1000)
+            const loginButton = await getElementById(driver, 'login-button')
+            await loginButton.click()
+            console.log('---> Credentials submitted')
 
-    await driver.wait(until.titleContains('VeloViewer'))
-    console.log('---> Returned to VeloViewer')
+        } catch (e) {
+            if (e.name === 'TimeoutError') {
+                console.log('---> No Login page found, already logged in')
+            } else {
+                throw e
+            }
+        }
+
+        await driver.wait(until.titleContains('VeloViewer'))
+        console.log('---> Returned to VeloViewer')
+    }
 }
